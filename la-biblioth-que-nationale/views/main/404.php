9076ec0db0ccd3f4f6deb3a0e3a5157f813c1bdf
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-05-24 14:17:52 +0200

    Update 404.php

--- la-biblioth-que-nationale/views/main/404.php
+++ la-biblioth-que-nationale/views/main/404.php
@@ -9,10 +9,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     
     <!-- fichier css unique à la page "404" -->
-    <link rel="stylesheet" href="/xampp/Labibliothèquenationale/la-biblioth-que-nationale/CSS/404.css">
+    <link rel="stylesheet" href="../../CSS/404.css">
 
     <!-- CSS global contenant quelques variables et des règles globales pour les pages -->
-    <link rel="stylesheet" href="/xampp/Labibliothèquenationale/la-biblioth-que-nationale/CSS/global.css">
+    <link rel="stylesheet" href="../../CSS/global.css">
 
     <!-- Polices google font API -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
